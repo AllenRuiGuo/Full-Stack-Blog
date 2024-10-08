b9544664d45b2d7d431bdc57ad13837a2067d2c3
Rui Guo <ruiguo.developer@outlook.com>
2024-10-08 17:47:15 +1000

    Use navbar from bootstrap

--- my-blog/src/NavBar.js
+++ my-blog/src/NavBar.js
@@ -6,36 +6,58 @@ const NavBar = () => {
   const { user } = useUser();
   const navigate = useNavigate();
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/articles">Articles</Link>
-        </li>
-      </ul>
-      <div className="nav-right">
-        {user ? (
-          <button
-            onClick={() => {
-              signOut(getAuth());
-            }}
-          >
-            Log out
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            Log In
-          </button>
-        )}
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <img className="brandimage" src="/favicon.svg" alt="MyBlog logo" />
+        </Link>
+
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav"
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="navbar-collapse collapse flex-grow-1 justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/articles">Articles</Link>
+            </li>
+          </ul>
+          <div>
+            {user ? (
+              <button
+                type="button"
+                onClick={() => {
+                  signOut(getAuth());
+                }}
+              >
+                Log out
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                Log In
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </nav>
   );
