a7da9a0cbd52be95189f0a391d7db998e3970d55
Rui Guo <ruiguo.developer@outlook.com>
2024-10-14 20:00:27 +1000

    Install popper.js and import bootstrap.bundle.min

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
