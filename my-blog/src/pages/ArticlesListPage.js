503c0b1e336088cd0a0d9fa9966b55d21eb519c5
Rui Guo <ruiguo.developer@outlook.com>
2024-10-20 16:26:18 +1000

    Change frontend to fetch Articles from MongoDB

--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -1,10 +1,33 @@
-import articles from "./article-content";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import ArticlesList from "../components/ArticlesList";
 
 const ArticlesListPage = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get("/api/articles");
+        setArticles(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error to fetch articles: ", error);
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <h1>Articles</h1>
+      <h1 className="text-center pb-3">Articles</h1>
       <ArticlesList articles={articles} />
     </>
   );
