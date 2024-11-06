6fdc38ad0b86deac49762d65d811a0f4c1b7083e
Rui Guo <ruiguo.developer@outlook.com>
2024-11-06 21:00:02 +1000

    Add a message when there is no comments

--- my-blog/src/components/CommentsList.js
+++ my-blog/src/components/CommentsList.js
@@ -6,13 +6,17 @@ const CommentsList = ({ comments }) => {
 
   return(
     <>
-      <h2 className="mb-3">Comments</h2>
-      {comments.map((comment) => (
-        <div className="comment" key={comment.postedBy + ": " + comment.text}>
-          <h6>{getDisplayName(comment.postedBy)}</h6>
-          <p>{comment.text}</p>
-        </div>
-      ))}
+      <h2 className="mb-4">Comments</h2>
+      {comments.length > 0 ? (
+        comments.map((comment) => (
+          <div className="comment" key={comment.postedBy + ": " + comment.text}>
+            <h6>{getDisplayName(comment.postedBy)}</h6>
+            <p>{comment.text}</p>
+          </div>
+        ))
+      ) : (
+        <h5 className="text-center">There is no comment yet.</h5>
+      )}
     </>
   );
 };
