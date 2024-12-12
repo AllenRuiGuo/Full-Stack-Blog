2c5f630a9a1e6a5d50997844d91108feeab61cec
Rui Guo <ruiguo.developer@outlook.com>
2024-12-13 08:46:42 +1000

    Add Footer component

---
+++ my-blog/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-white py-3 mt-auto">
+            <div className="container text-center">
+                <p>&copy;2024 MyDigitool. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
