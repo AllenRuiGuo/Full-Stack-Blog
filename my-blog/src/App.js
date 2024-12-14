a4da9f69b67046862d8ff1df2952fa328d9eb80e
Rui Guo <ruiguo.developer@outlook.com>
2024-12-14 11:36:11 +1000

    Style the page body, makes it take up all the space between nav and footer

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -13,7 +13,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App" id="page-app">
         <NavBar />
         <div id="page-body">
           <Routes>
